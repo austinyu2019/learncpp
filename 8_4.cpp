5617d2176cee4d0d3def17fd02e80ddc258c4c0e
austinyu <geniusyml@gmail.com>
2019-10-29 22:41:50 +0800

    第八周作业

---
+++ 8_4.cpp
@@ -0,0 +1,29 @@
+//编程题＃4：找和为K的两个元素
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0, k = 0;
+	int a[1000];
+
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[i] + a[j] == k)
+			{
+				cout << "yes" << endl;
+				return 0;
+			}
+		}
+	}
+	cout << "no" << endl;
+	return 0;
+}
